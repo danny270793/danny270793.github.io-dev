c8052118158bd6da0023c92b041673bcca9c6f3c
Danny Vaca <danny270793@icloud.com>
2024-08-29 00:02:48 -0500

    change project types

--- src/libraries/me/knowledges/projects.ts
+++ src/libraries/me/knowledges/projects.ts
@@ -1,5 +1,5 @@
 export interface Project {
-  type: "mobile";
+  type: 'playstore';
   name: string;
   description: string;
   link: string;
@@ -7,26 +7,26 @@ export interface Project {
 
 const projects: Project[] = [
   {
-    type: "mobile",
+    type: "playstore",
     name: "Sppedometer",
     description: "App to measure the speed which the device is moving on",
     link: "https://play.google.com/store/apps/details?id=io.github.danny270793.sppedometer",
   },
   {
-    type: "mobile",
+    type: "playstore",
     name: "Soundmeter",
     description:
       "App to measure the strengh of the noise which is reaching your phone microphone",
     link: "https://play.google.com/store/apps/details?id=io.github.danny270793.soundmeter.soundmeter",
   },
   {
-    type: "mobile",
+    type: "playstore",
     name: "Mycompass",
     description: "App to measure the orientation of the device",
     link: "https://play.google.com/store/apps/details?id=io.github.danny270793.mycompass",
   },
   {
-    type: "mobile",
+    type: "playstore",
     name: "Maint Azure Monitoring",
     description: "App get insights (prices, ussage) from azure accout",
     link: "https://play.google.com/store/apps/details?id=com.maintlatam.azure.pipelinesmonitoring.pipelines_monitoring",
