8e0ea9cb65477389fadd8d8e20b3100d5346ee16
Danny Vaca <danny270793@icloud.com>
2024-08-29 00:03:07 -0500

    deatils about me

---
+++ src/libraries/me/index.ts
@@ -0,0 +1,8 @@
+export interface Me {
+    detail: string
+}
+
+const me: Me = {
+    detail: 'Senior Full Stack Developer with almost 10 years of experience implementing solutions for digital transformation in the cloud (Azure, AWS), developing cross-platform mobile applications (Flutter, React Native) as well as deploying web applications based on microservices (Java, .Net, Python)\nI have a wide range of certifications in Cloudera, Kubernetes, Azure, Terraform, Java, Python, UiPath that guarantee my knowledge\nMy previous experience allows me to understand and anticipate challenges, as well as propose solutions that add value to the business and ensure return on investment.'
+}
+export default me
