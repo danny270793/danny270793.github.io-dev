dba181a1cf7332d97cf578efa683332e8266e5f4
Danny Vaca <danny270793@gmail.com>
2024-08-29 12:25:47 -0500

    format code

--- src/libraries/me/knowledges/projects.ts
+++ src/libraries/me/knowledges/projects.ts
@@ -1,35 +1,39 @@
 export interface Project {
   name: string;
   description: string;
-  playstore: string|undefined;
-  appstore: string|undefined;
+  playstore: string | undefined;
+  appstore: string | undefined;
 }
 
 const projects: Project[] = [
   {
     name: "Sppedometer",
     description: "App to measure the speed which the device is moving on",
-    playstore: "https://play.google.com/store/apps/details?id=io.github.danny270793.sppedometer",
-    appstore: undefined
+    playstore:
+      "https://play.google.com/store/apps/details?id=io.github.danny270793.sppedometer",
+    appstore: undefined,
   },
   {
     name: "Soundmeter",
     description:
       "App to measure the strengh of the noise which is reaching your phone microphone",
-      playstore: "https://play.google.com/store/apps/details?id=io.github.danny270793.soundmeter.soundmeter",
-    appstore: undefined
+    playstore:
+      "https://play.google.com/store/apps/details?id=io.github.danny270793.soundmeter.soundmeter",
+    appstore: undefined,
   },
   {
     name: "Mycompass",
     description: "App to measure the orientation of the device",
-    playstore: "https://play.google.com/store/apps/details?id=io.github.danny270793.mycompass",
-    appstore: undefined
+    playstore:
+      "https://play.google.com/store/apps/details?id=io.github.danny270793.mycompass",
+    appstore: undefined,
   },
   {
     name: "Maint Azure Monitoring",
     description: "App get insights (prices, ussage) from azure accout",
-    playstore: "https://play.google.com/store/apps/details?id=com.maintlatam.azure.pipelinesmonitoring.pipelines_monitoring",
-    appstore: undefined
+    playstore:
+      "https://play.google.com/store/apps/details?id=com.maintlatam.azure.pipelinesmonitoring.pipelines_monitoring",
+    appstore: undefined,
   },
 ];
 export default projects;
