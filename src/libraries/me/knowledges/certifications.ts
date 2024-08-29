0fe7f4c56c0f4751462407a49e1bd213dc8ea6a6
Danny Vaca <danny270793@gmail.com>
2024-08-29 00:02:59 -0500

    add more certifications

--- src/libraries/me/knowledges/certifications.ts
+++ src/libraries/me/knowledges/certifications.ts
@@ -1,4 +1,7 @@
 import AzureCosmosSpeciality from "../../../../public/images/certifications/20221028 - Azure Cosmos DB Developer Specialty.png";
+import UiPath from "../../../../public/images/certifications/20190607 - RPA Developer Certificate of completion.png";
+import Arduino from "../../../../public/images/certifications/20200308 - Arduino Fundamentals.png";
+import CCASparkAndHadoopDeveloper from "../../../../public/images/certifications/20201108 - CCA Spark And Hadoop Developer.png";
 import { ImageMetadata } from "astro";
 
 export interface Certification {
@@ -6,6 +9,7 @@ export interface Certification {
   name: string;
   brand: string;
   link: string;
+  date: Date
   image: ImageMetadata;
 }
 
@@ -15,7 +19,32 @@ const certifications: Certification[] = [
     name: "Microsoft Certified: Azure Cosmos DB Developer Specialty",
     code: "DP-420",
     brand: "Microsoft",
+    date: new Date('2022/10/28'),
     image: AzureCosmosSpeciality,
   },
+  {
+    link: 'https://academy.uipath.com/certification',
+    name: 'RPA Developer Certificate of completion',
+    brand: 'UiPath',
+    code: '',
+    date: new Date('2019/06/07'),
+    image: UiPath,
+  },
+  {
+    link: 'https://certifications-assets-cf.arduino.cc/certificates/a8d0ed84-3a1c-4b71-9985-c44486ac0e28.pdf',
+    name: 'Arduino Fundamentals',
+    brand: 'Arduino',
+    code: '',
+    date: new Date('2020/03/08'),
+    image: Arduino
+  },
+  {
+    link: '',
+    name: 'CCA Spark and Hadoop Developer',
+    brand: 'Cloudera',
+    code: '',
+    date: new Date('2020/11/08'),
+    image: CCASparkAndHadoopDeveloper
+  }
 ];
 export default certifications;
