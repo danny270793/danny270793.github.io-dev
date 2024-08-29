8a2b630fa7a741a72e4628ef0236c868136ff0da
Danny Vaca <danny270793@icloud.com>
2024-08-29 12:23:59 -0500

    improve ui

--- src/libraries/me/knowledges/projects.ts
+++ src/libraries/me/knowledges/projects.ts
@@ -1,35 +1,35 @@
 export interface Project {
-  type: "playstore";
   name: string;
   description: string;
-  link: string;
+  playstore: string|undefined;
+  appstore: string|undefined;
 }
 
 const projects: Project[] = [
   {
-    type: "playstore",
     name: "Sppedometer",
     description: "App to measure the speed which the device is moving on",
-    link: "https://play.google.com/store/apps/details?id=io.github.danny270793.sppedometer",
+    playstore: "https://play.google.com/store/apps/details?id=io.github.danny270793.sppedometer",
+    appstore: undefined
   },
   {
-    type: "playstore",
     name: "Soundmeter",
     description:
       "App to measure the strengh of the noise which is reaching your phone microphone",
-    link: "https://play.google.com/store/apps/details?id=io.github.danny270793.soundmeter.soundmeter",
+      playstore: "https://play.google.com/store/apps/details?id=io.github.danny270793.soundmeter.soundmeter",
+    appstore: undefined
   },
   {
-    type: "playstore",
     name: "Mycompass",
     description: "App to measure the orientation of the device",
-    link: "https://play.google.com/store/apps/details?id=io.github.danny270793.mycompass",
+    playstore: "https://play.google.com/store/apps/details?id=io.github.danny270793.mycompass",
+    appstore: undefined
   },
   {
-    type: "playstore",
     name: "Maint Azure Monitoring",
     description: "App get insights (prices, ussage) from azure accout",
-    link: "https://play.google.com/store/apps/details?id=com.maintlatam.azure.pipelinesmonitoring.pipelines_monitoring",
+    playstore: "https://play.google.com/store/apps/details?id=com.maintlatam.azure.pipelinesmonitoring.pipelines_monitoring",
+    appstore: undefined
   },
 ];
 export default projects;
