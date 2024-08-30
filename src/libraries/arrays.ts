58bdb7d343a9d189fac050675fbb92064c52766f
Danny Vaca <danny270793@gmail.com>
2024-08-29 21:15:17 -0500

    format coe

--- src/libraries/arrays.ts
+++ src/libraries/arrays.ts
@@ -1,8 +1,12 @@
-Array.prototype.chunk = function (chunkSize) {
-  let result = [];
+interface Array<T> {
+    chunk(chunkSize: number): Array<T[]>;
+}
+
+Array.prototype.chunk = function<T>(chunkSize: number): Array<T[]> {
+  const result: Array<T[]> = [];
 
   for (let i = 0; i < this.length; i += chunkSize) {
-    let chunk = this.slice(i, i + chunkSize);
+    const chunk: T[] = this.slice(i, i + chunkSize);
     result.push(chunk);
   }
 
