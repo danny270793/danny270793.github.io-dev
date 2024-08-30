99b7d34c25ad748f25cec91ee9749ed357d6a372
Danny Vaca <danny270793@gmail.com>
2024-08-29 21:13:40 -0500

    format code

--- src/libraries/arrays.ts
+++ src/libraries/arrays.ts
@@ -1,10 +1,10 @@
 Array.prototype.chunk = function (chunkSize) {
-    let result = [];
-  
-    for (let i = 0; i < this.length; i += chunkSize) {
-      let chunk = this.slice(i, i + chunkSize);
-      result.push(chunk);
-    }
-  
-    return result;
+  let result = [];
+
+  for (let i = 0; i < this.length; i += chunkSize) {
+    let chunk = this.slice(i, i + chunkSize);
+    result.push(chunk);
+  }
+
+  return result;
 };
