c8d9e12d75ad75ea67cf4156b6cd74a06947e20d
Danny Vaca <danny270793@icloud.com>
2024-08-29 21:43:12 -0500

    add more certifications

--- src/libraries/me/knowledges/certifications.ts
+++ src/libraries/me/knowledges/certifications.ts
@@ -2,6 +2,9 @@ import AzureCosmosSpeciality from "../../../../public/images/certifications/2022
 import UiPath from "../../../../public/images/certifications/20190607 - RPA Developer Certificate of completion.png";
 import Arduino from "../../../../public/images/certifications/20200308 - Arduino Fundamentals.png";
 import CCASparkAndHadoopDeveloper from "../../../../public/images/certifications/20201108 - CCA Spark And Hadoop Developer.png";
+import OracleJava from "../../../../public/images/certifications/20210426 - Oracle Certified Professional Java SE 11 Developer.png";
+import MET from "../../../../public/images/certifications/20201113 - met.png";
+import Python from "../../../../public/images/certifications/20210523 - Certified Entry-Level Python Programmer.png";
 import { ImageMetadata } from "astro";
 
 export interface Certification {
@@ -14,14 +17,6 @@ export interface Certification {
 }
 
 const certifications: Certification[] = [
-  {
-    link: "https://www.credly.com/badges/a8fd23c4-72d4-4fd3-85fd-f82584b764b7/public_url",
-    name: "Microsoft Certified: Azure Cosmos DB Developer Specialty",
-    code: "DP-420",
-    brand: "Microsoft",
-    date: new Date("2022/10/28"),
-    image: AzureCosmosSpeciality,
-  },
   {
     link: "https://academy.uipath.com/certification",
     name: "RPA Developer Certificate of completion",
@@ -45,6 +40,40 @@ const certifications: Certification[] = [
     code: "",
     date: new Date("2020/11/08"),
     image: CCASparkAndHadoopDeveloper,
+  },
+  {
+    link: "https://www.credly.com/badges/a8fd23c4-72d4-4fd3-85fd-f82584b764b7/public_url",
+    name: "B1 MET",
+    code: "",
+    brand: "MET",
+    date: new Date("2020/11/23"),
+    image: MET,
+  },
+  {
+    link: "https://www.credly.com/badges/449fcbf9-16c3-4949-a0cb-18d35278a845/public_url",
+    name: "Oracle Certified Professional Java SE 11 Developer",
+    code: "1Z0-816",
+    brand: "Oracle",
+    date: new Date("2021/04/26"),
+    image: OracleJava,
+  },
+  {
+    link: "https://www.credly.com/badges/3e9780b1-9544-4b8c-834a-87be9e84d24f/public_url",
+    name: "Certified Entry-Level Python Programmer",
+    code: "PCEP",
+    brand: "Python Institute",
+    date: new Date("2021/05/23"),
+    image: Python,
+  },
+  
+
+  {
+    link: "https://www.credly.com/badges/a8fd23c4-72d4-4fd3-85fd-f82584b764b7/public_url",
+    name: "Microsoft Certified: Azure Cosmos DB Developer Specialty",
+    code: "DP-420",
+    brand: "Microsoft",
+    date: new Date("2022/10/28"),
+    image: AzureCosmosSpeciality,
   },
 ];
 export default certifications;
