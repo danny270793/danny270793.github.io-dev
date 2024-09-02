d07b451d1f675c8114b203fe624b944d144c2973
Danny Vaca <danny270793@icloud.com>
2024-09-02 11:06:34 -0500

    format code

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://danny270793.github.io",
   build: {
-    assets: 'astro'
-  }
+    assets: "astro",
+  },
 });
