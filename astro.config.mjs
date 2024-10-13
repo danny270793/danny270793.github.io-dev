6b7858c07f0a4df7b75aa592d052ff215cb604db
Danny Vaca <danny270793@gmail.com>
2024-10-13 14:07:34 -0500

    add preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://danny270793.github.io",
   build: {
-    assets: "astro",
+    assets: "astro"
   },
+  integrations: [preact()]
 });
